84a38b01b5fd5a0631a2f3e431fc414cfab0c521
MayteGS <may08jul@hotmail.com>
2019-09-24 11:47:48 -0500

    Ejercicio 1 Se creo el HTML y JS

---
+++ Arrays/Ejercicio1/app.js
@@ -0,0 +1,13 @@
+let btnCalcular = document.getElementById('btnCalcular');
+let tipoOperacion = document.getElementById('tipoOperacion');
+let contador = 0;
+
+EventListener();
+
+function EventListener() {
+    btnCalcular.addEventListener('click', calcularArray);
+}
+
+function calcularArray() {
+
+}
